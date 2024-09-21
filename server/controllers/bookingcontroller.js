4a20d11b9d19cf4dc3133f777b9a3759bd44c4c2
dharaneesh1212 <dharun12122002@gmail.com>
2024-09-21 11:31:07 +0530

    server-c-r

--- server/controllers/bookingcontroller.js
+++ server/controllers/bookingcontroller.js
@@ -1,10 +1,57 @@
 import Booking from "../models/bookingmodel.js";
 
+// create
 export const create = async (req, res) => {
   try {
     const turf = new Booking(req.body);
     await turf.save();
     res.status(201).json({ success: true, data: turf });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// get all
+export const allBookings = async (req, res) => {
+  try {
+    const getall = await Booking.find();
+    res.status(200).json({ success: true, data: getall });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// get by name
+export const bookingsByName = async (req, res) => {
+  try {
+    const byName = await Booking.find({ username: req.params.username });
+    if (!byName.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No Bookings found",
+      });
+    }
+    res.status(200).json({ success: true, data: byName });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// delete
+export const deleteBooking = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteSlot = await Booking.findByIdAndDelete(id);
+    if (!deleteSlot) {
+      res.status(404).json({
+        success: false,
+        message: error.message,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Booking deleted successfully",
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
--- server/routes/bookingroute.js
+++ server/routes/bookingroute.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { create } from "../controllers/bookingcontroller.js";
+import { allBookings, bookingsByName, create, deleteBooking } from "../controllers/bookingcontroller.js";
 
 const router = express.Router();
 
 // POST: http://localhost:8000/turf/slots
 router.post("/slots", create);
+
+// GET: http://localhost:8000/turf/all
+router.post("/all", allBookings);
+
+// GET: http://localhost:8000/turf/getbyname
+router.post("/getbyname", bookingsByName);
+
+// DELETE: http://localhost:8000/turf/delete
+router.post("/delete", deleteBooking);
 
 export default router;
