9a54d3897189059da4caae33227d751a320082dd
dharaneesh1212 <dharun12122002@gmail.com>
2024-09-27 12:00:42 +0530

    client-images

---
+++ client/src/components/home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main className="h-screen w-screen flex justify-center items-center">
+      <div>
+        
+      </div>
+    </main>
+  );
+};
+
+export default Home;
