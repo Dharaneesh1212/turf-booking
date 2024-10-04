b9937441aeb75c43baf69ce84ba67a5e4b4e9413
dharaneesh1212 <dharun12122002@gmail.com>
2024-10-04 12:28:38 +0530

    days

--- client/src/days/monday.jsx
+++ client/src/days/monday.jsx
@@ -5,18 +5,26 @@ const Monday = () => {
     <main className="flex items-center justify-center">
       <div className="flex items-center justify-start border-2 border-black h-[20rem] w-[30rem] rounded-md flex-col gap-6">
         <div className="flex justify-center items-center w-full rounded-md bg-green-300">
-          <img src="" alt="" className="bg-zinc-600 h-[13rem]" />
+          <img
+            src=""
+            alt=""
+            className="animate__animated animate__zoomIn bg-zinc-600 h-[13rem]"
+          />
         </div>
         <div className="flex items-center justify-between flex-row w-[28rem]">
           <div className="flex items-center justify-center flex-col gap-4">
-            <span className="text-lg font-medium">Date : </span>
-            <span className="text-lg font-medium">Day : </span>
+            <span className="text-lg font-medium animate__animated animate__zoomIn">
+              Date :{" "}
+            </span>
+            <span className="text-lg font-medium animate__animated animate__zoomIn">
+              Day :{" "}
+            </span>
           </div>
           <div>
             <Link>
               <button
                 id="ofr-btn"
-                className="p-2 rounded-md text-lg font-medium text-white bg-red-500 hover:bg-red-600"
+                className="p-2 rounded-md text-lg font-medium animate__animated animate__zoomIn text-white bg-red-500 hover:bg-red-600"
               >
                 Book Slot
               </button>
