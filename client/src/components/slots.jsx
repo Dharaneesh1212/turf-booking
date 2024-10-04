4176530f40bb485e1637243f6fb9a5301007f80c
dharaneesh1212 <dharun12122002@gmail.com>
2024-10-04 12:40:53 +0530

    slots

--- client/src/components/slots.jsx
+++ client/src/components/slots.jsx
@@ -1,22 +1,219 @@
-import Monday from "../days/monday";
-import Tuesday from "../days/tuesday";
-import Wednesday from "../days/wednesday";
-import Thursday from "../days/thursday";
-import Friday from "../days/friday";
-import Saturday from "../days/saturday";
-import Sunday from "../days/sunday";
+import { Link } from "react-router-dom";
 
 const Slots = () => {
   return (
     <main className="flex items-center justify-center">
       <div className="flex flex-wrap justify-center items-center gap-24 p-4 h-full w-full">
-        <Monday />
-        <Tuesday />
-        <Wednesday />
-        <Thursday />
-        <Friday />
-        <Saturday />
-        <Sunday />
+        {/* one */}
+        <div className="flex items-center justify-start border-2 border-black h-[20rem] w-[30rem] rounded-md flex-col gap-6">
+          <div className="flex justify-center items-center w-full rounded-md bg-green-300">
+            <img
+              src=""
+              alt=""
+              className="animate__animated animate__zoomIn bg-zinc-600 h-[13rem]"
+            />
+          </div>
+          <div className="flex items-center justify-between flex-row w-[28rem]">
+            <div className="flex items-center justify-center flex-col gap-4">
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Date :{" "}
+              </span>
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Day :{" "}
+              </span>
+            </div>
+            <div>
+              <Link to="/monday">
+                <button
+                  id="ofr-btn"
+                  className="p-2 rounded-md text-lg font-medium animate__animated animate__zoomIn text-white bg-red-500 hover:bg-red-600"
+                >
+                  Book Slot
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* two */}
+        <div className="flex items-center justify-start border-2 border-black h-[20rem] w-[30rem] rounded-md flex-col gap-6">
+          <div className="flex justify-center items-center w-full rounded-md bg-green-300">
+            <img
+              src=""
+              alt=""
+              className="animate__animated animate__zoomIn bg-zinc-600 h-[13rem]"
+            />
+          </div>
+          <div className="flex items-center justify-between flex-row w-[28rem]">
+            <div className="flex items-center justify-center flex-col gap-4">
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Date :{" "}
+              </span>
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Day :{" "}
+              </span>
+            </div>
+            <div>
+              <Link to="/tuesday">
+                <button
+                  id="ofr-btn"
+                  className="p-2 rounded-md text-lg font-medium animate__animated animate__zoomIn text-white bg-red-500 hover:bg-red-600"
+                >
+                  Book Slot
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* three */}
+        <div className="flex items-center justify-start border-2 border-black h-[20rem] w-[30rem] rounded-md flex-col gap-6">
+          <div className="flex justify-center items-center w-full rounded-md bg-green-300">
+            <img
+              src=""
+              alt=""
+              className="animate__animated animate__zoomIn bg-zinc-600 h-[13rem]"
+            />
+          </div>
+          <div className="flex items-center justify-between flex-row w-[28rem]">
+            <div className="flex items-center justify-center flex-col gap-4">
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Date :{" "}
+              </span>
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Day :{" "}
+              </span>
+            </div>
+            <div>
+              <Link to="/wednesday">
+                <button
+                  id="ofr-btn"
+                  className="p-2 rounded-md text-lg font-medium animate__animated animate__zoomIn text-white bg-red-500 hover:bg-red-600"
+                >
+                  Book Slot
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* four */}
+        <div className="flex items-center justify-start border-2 border-black h-[20rem] w-[30rem] rounded-md flex-col gap-6">
+          <div className="flex justify-center items-center w-full rounded-md bg-green-300">
+            <img
+              src=""
+              alt=""
+              className="animate__animated animate__zoomIn bg-zinc-600 h-[13rem]"
+            />
+          </div>
+          <div className="flex items-center justify-between flex-row w-[28rem]">
+            <div className="flex items-center justify-center flex-col gap-4">
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Date :{" "}
+              </span>
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Day :{" "}
+              </span>
+            </div>
+            <div>
+              <Link to="/thursday">
+                <button
+                  id="ofr-btn"
+                  className="p-2 rounded-md text-lg font-medium animate__animated animate__zoomIn text-white bg-red-500 hover:bg-red-600"
+                >
+                  Book Slot
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* five */}
+        <div className="flex items-center justify-start border-2 border-black h-[20rem] w-[30rem] rounded-md flex-col gap-6">
+          <div className="flex justify-center items-center w-full rounded-md bg-green-300">
+            <img
+              src=""
+              alt=""
+              className="animate__animated animate__zoomIn bg-zinc-600 h-[13rem]"
+            />
+          </div>
+          <div className="flex items-center justify-between flex-row w-[28rem]">
+            <div className="flex items-center justify-center flex-col gap-4">
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Date :{" "}
+              </span>
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Day :{" "}
+              </span>
+            </div>
+            <div>
+              <Link to="/friday">
+                <button
+                  id="ofr-btn"
+                  className="p-2 rounded-md text-lg font-medium animate__animated animate__zoomIn text-white bg-red-500 hover:bg-red-600"
+                >
+                  Book Slot
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* six */}
+        <div className="flex items-center justify-start border-2 border-black h-[20rem] w-[30rem] rounded-md flex-col gap-6">
+          <div className="flex justify-center items-center w-full rounded-md bg-green-300">
+            <img
+              src=""
+              alt=""
+              className="animate__animated animate__zoomIn bg-zinc-600 h-[13rem]"
+            />
+          </div>
+          <div className="flex items-center justify-between flex-row w-[28rem]">
+            <div className="flex items-center justify-center flex-col gap-4">
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Date :{" "}
+              </span>
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Day :{" "}
+              </span>
+            </div>
+            <div>
+              <Link to="/saturday">
+                <button
+                  id="ofr-btn"
+                  className="p-2 rounded-md text-lg font-medium animate__animated animate__zoomIn text-white bg-red-500 hover:bg-red-600"
+                >
+                  Book Slot
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* seven */}
+        <div className="flex items-center justify-start border-2 border-black h-[20rem] w-[30rem] rounded-md flex-col gap-6">
+          <div className="flex justify-center items-center w-full rounded-md bg-green-300">
+            <img
+              src=""
+              alt=""
+              className="animate__animated animate__zoomIn bg-zinc-600 h-[13rem]"
+            />
+          </div>
+          <div className="flex items-center justify-between flex-row w-[28rem]">
+            <div className="flex items-center justify-center flex-col gap-4">
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Date :{" "}
+              </span>
+              <span className="text-lg font-medium animate__animated animate__zoomIn">
+                Day :{" "}
+              </span>
+            </div>
+            <div>
+              <Link to="/sunday">
+                <button
+                  id="ofr-btn"
+                  className="p-2 rounded-md text-lg font-medium animate__animated animate__zoomIn text-white bg-red-500 hover:bg-red-600"
+                >
+                  Book Slot
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
